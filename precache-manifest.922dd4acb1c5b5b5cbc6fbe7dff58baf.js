d34dce5a5cdd80a8fd076388a26f73a125fa886e
Ivan Rendon <ivansito87@gmail.com>
2019-06-18 15:33:34 -0400

    Updates

---
+++ precache-manifest.922dd4acb1c5b5b5cbc6fbe7dff58baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b927859ec4fe6617cca7d6d45b2ff7c",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "c65e56f9bade6009aa69",
+    "url": "/Clicky_Game/static/css/main.f5cc2526.chunk.css"
+  },
+  {
+    "revision": "fc5b3d13895358969f86",
+    "url": "/Clicky_Game/static/js/2.9135df07.chunk.js"
+  },
+  {
+    "revision": "c65e56f9bade6009aa69",
+    "url": "/Clicky_Game/static/js/main.7f7fb8b8.chunk.js"
+  },
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  }
+]);
